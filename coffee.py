cca623c5259b6e606f6b4641dfc5e238d74e5e3f
Andrey Kropotin <tork713@gmail.com>
2020-06-27 09:10:17 +0700

    заготовка

--- coffee.py
+++ coffee.py
@@ -1,1 +1,22 @@
-print('Hello')
+number_of_cups = int(input("Write how many cups of coffee you will need:\n"))
+print("For ", number_of_cups, " cups of coffee you will need:\n",
+      number_of_cups * 200, " ml of water\n", number_of_cups * 50,
+      " ml of milk\n", number_of_cups * 15, " g of coffee beans")
+
+Write how many ml of water the coffee machine has:
+> 300
+Write how many ml of milk the coffee machine has:
+> 65
+Write how many grams of coffee beans the coffee machine has:
+> 100
+Write how many cups of coffee you will need:
+> 1
+Yes, I can make that amount of coffee
+
+No, I can make only 2 cups of coffee
+
+No, I can make only 0 cups of coffee
+
+Yes, I can make that amount of coffee
+
+Yes, I can make that amount of coffee (and even 1 more than that)
